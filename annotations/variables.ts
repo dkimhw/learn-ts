ebedfaaf6fb6d4858715f6f1b953709c25c23636
David Kim <dkhw90@gmail.com>
2024-08-02 17:48:03 +0200

    Practice with typescript annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -25,6 +25,31 @@ let dog: { firstName: string; lastName: string} = {
 }
 
 // Function
-const logNumber = (i: number) =>  {
+const logNumber: (i: number) => void = (i: number) =>  {
   console.log(i);
+}
+
+// when function returns "any" type
+const json = '{"x": 10, "y": 20}';
+const coordinates: {x: number, y: number} = JSON.parse(json); // TS is implicitly returning "any"
+console.log(coordinates);
+
+// when we declare a variable on one line
+let words = ['red', 'gren', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false; // can be a number or boolean
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i]; // TS believes it should be boolean always so need type annotation
+  }
 }
