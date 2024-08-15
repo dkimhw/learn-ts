030b9fdcfb5ea4e25c334d2e4e537f78c6d07b16
David Kim <dkhw90@gmail.com>
2024-08-15 21:50:16 +0200

    Work with class attributes & constructors

--- classes-practice/classes.ts
+++ classes-practice/classes.ts
@@ -1,22 +1,46 @@
 
 
 class Vehicle {
-  drive(): void {
+  color: string;
+
+  constructor (color: string) {
+    this.color = color;
+  }
+
+  public drive(): void {
     console.log('chugga chugga');
   }
 
-  honk(): void {
+  // only accessible within the child class that extends Vehicle
+  protected honk(): void {
     console.log('beep');
   }
 }
 
+const v = new Vehicle('red');
+console.log(v.color);
+
 class NewCar extends Vehicle {
+  constructor (public wheels: number, color: string) {
+    super(color);
+    this.wheels = wheels;
+  }
+
   // override parent class method
   drive(): void {
     console.log('vroom');
   }
+
+  // can set new methods
+  // when marking methods as private is to restrict the different methods that other developers
+  // can call.
+  public park(): void {
+    this.honk();
+    console.log('i am parking!');
+  }
 }
 
-let newCar = new NewCar();
+// constructor of vehicle is being called so need to pass in a string color val
+let newCar = new NewCar(4, 'red');
 newCar.drive();
-newCar.honk();
+newCar.park();
