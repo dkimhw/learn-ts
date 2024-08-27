7c15ecc7f363390ed250265e41962e908e922992
David Kim <dkhw90@gmail.com>
2024-08-27 19:48:31 +0200

    Add content text to mappable interface

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -8,7 +8,8 @@ interface Mappable {
   location: {
     lat: number,
     lng: number
-  }
+  };
+  markerContent(): string;
 }
 
 export class CustomMap {
@@ -26,7 +27,7 @@ export class CustomMap {
     try {
       const { Map } = await this.googleMapLoader.importLibrary("maps") as google.maps.MapsLibrary;
       const map = new Map(document.getElementById(divId) as HTMLElement, {
-        center: center, zoom: 1.75, mapId: '1'
+        center: center, zoom: 1.5, mapId: '1'
       });
       this.map = map;
     } catch (e) {
@@ -37,10 +38,18 @@ export class CustomMap {
   async addMarker(mappable: Mappable): Promise<void> {
     try {
       const { AdvancedMarkerElement } = await this.googleMapLoader.importLibrary("marker");
-      new AdvancedMarkerElement({
+      const marker = new AdvancedMarkerElement({
         map: this.map,
         position: { lat: mappable.location.lat, lng: mappable.location.lng },
-    });
+      });
+
+      marker.addListener('click', () => {
+        const infoWindow = new google.maps.InfoWindow({
+          content: mappable.markerContent()
+        });
+
+        infoWindow.open(this.map, marker);
+      })
     } catch (e) {
       throw new Error(e);
     }
