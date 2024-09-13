e796e4290d2c60d9ce6c789b33f02479533e24f4
David Kim <dkhw90@gmail.com>
2024-09-13 20:40:02 +0200

    Create a bad way to write reusable sort code in TS

---
+++ sort/src/BadSort.ts
@@ -0,0 +1,29 @@
+
+// Bad solution using type guards because your code gets big trying to account
+// for all types of collections
+class BadSorter {
+  constructor(public collection: number[] | string) {
+    this.collection = collection;
+  }
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+
+        // type guard
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            let tmp = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = tmp;
+          }
+        }
+
+        if (typeof this.collection === 'string') {
+        }
+      }
+    }
+  }
+}
