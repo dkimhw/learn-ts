3fd6d6a34aecfa68c36b397eb8a14e7b026d8c06
David Kim <dkhw90@gmail.com>
2024-09-29 21:36:09 +0200

    Refactor csv reader using generics to allow for different types of data inputs

--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -3,7 +3,9 @@ import { CsvFileReader } from "./CsvFileReader";
 import { dateStringToDate } from "./utils";
 import { MatchResult } from "./MatchResult";
 
-export class MatchReader extends CsvFileReader {
+type MatchData = [Date, string, string, number, number, MatchResult, string]
+
+export class MatchReader extends CsvFileReader<MatchData> {
   constructor(public filename: string) {
     super(filename);
   }
